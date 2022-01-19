03f71f743a15aba3fce4c5f4542d5df00ccd336c
Agbo-Frank <agbofrank531@gmail.com>
2022-01-19 23:15:38 +0100

    first commit

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.use(express.json())
 app.use(express.json({ limit: '50mb'}))
 app.use(express.urlencoded({limit: '50mb', extended: true}))
 
+const db = process.env.MONGODB_URL
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+ .then(() => app.listen(PORT, () => console.log('listening to port 5500')))
+ .catch(err => console.log(err))
+
 app.use('/graphql', authenticate, graphqlHTTP({
     schema,
     rootValue,
@@ -25,11 +30,6 @@ app.use('/graphql', authenticate, graphqlHTTP({
 }))
 
 app.use('/upload', auth, upload)
-
-const db = process.env.MONGODB_URL
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
- .then(() => app.listen(PORT, () => console.log('listening to port 5500')))
- .catch(err => console.log(err))
 
  if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.resolve(__dirname, './client/build')))
